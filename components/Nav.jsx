50ee8420db7bb9d5ee99b5814bb15dcb8eb62e08
Jwill617Boston <77422031+Jwill617Boston@users.noreply.github.com>
2023-05-16 00:24:44 -0400

    Nav 2

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,11 +1,30 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
 import { useState, useEffect } from "react";
-import { signIn, signOut, useSession, getProviders} from 'next-auth/react'
-
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-   return <h1>Nav</h1>;
+   return (
+      <nav className="flex-between w-full mb-16 pt-3">
+         <Link href="/" className="flex gap-2 flex-center">
+            <Image
+               src="/assets/images/logo.svg"
+               alt="Logo"
+               width={30}
+               height={30}
+               className="object-contain"
+            />
+         </Link>
+         <Link href="/" className="flex gap-2 flex-center">
+            Home
+         </Link>
+         <Link href="/" className="flex gap-2 flex-center">
+            Home
+         </Link>
+      </nav>
+   );
 };
 
 export default Nav;
