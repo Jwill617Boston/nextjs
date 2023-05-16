95f7d7a9d2e093940565345ac0c3d485f312075b
Jwill617Boston <77422031+Jwill617Boston@users.noreply.github.com>
2023-05-16 00:11:58 -0400

    Nav Start

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,5 +1,11 @@
+import Link from "next/link";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import { signIn, signOut, useSession, getProviders} from 'next-auth/react'
+
+
 const Nav = () => {
-   return <div>Nav</div>;
+   return <h1>Nav</h1>;
 };
 
 export default Nav;
