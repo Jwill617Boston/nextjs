d0904e7e4f69929b3cf04d5f95d6e325172a587f
Jwill617Boston <77422031+Jwill617Boston@users.noreply.github.com>
2023-05-16 01:34:14 -0400

    Create page

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,6 +6,7 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
+   const isUserLoggedIn = true;
    return (
       <nav className="flex-between w-full mb-16 pt-3">
          <Link href="/" className="flex gap-2 flex-center">
@@ -17,12 +18,18 @@ const Nav = () => {
                className="object-contain"
             />
          </Link>
-         <Link href="/" className="flex gap-2 flex-center">
-            Link 1
-         </Link>
-         <Link href="/" className="flex gap-2 flex-center">
-            Link 2
-         </Link>
+
+         <div className="sm:flex hidden">
+            {isUserLoggedIn ? (
+               <div className="flex gap-3 md:gap-5">
+                  <Link href="/create-prompt" className="black_btn">
+                     Create Post
+                  </Link>
+               </div>
+            ) : (
+               <></>
+            )}
+         </div>
       </nav>
    );
 };
